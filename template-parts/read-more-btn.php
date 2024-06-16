75729014961e945d827748b07f0cad202bb4d6c5
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2024-06-16 16:48:07 +0300

    main page start

--- template-parts/read-more-btn.php
+++ template-parts/read-more-btn.php
@@ -1,10 +1,10 @@
 <div class="readmore_button">
-  <button type="button">Читати все</button>
-  <div class="readmore_button--box">
-    <div class="readmore_button--innerbox">
-      <svg class="readmore_button--icon">
-        <use href="<?php echo get_template_directory_uri()?>/assets/images/sprite.svg#arrow_down_long"></use>
-      </svg>
+    <button type="button">Читати все</button>
+    <div class="readmore_button--box">
+        <div class="readmore_button--innerbox">
+            <svg class="readmore_button--icon">
+                <use href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#arrow_down_long"></use>
+            </svg>
+        </div>
     </div>
-  </div>
 </div>
