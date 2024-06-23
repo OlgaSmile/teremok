dc8dc95f8349a5cba11f2ead71bd418eb1313c6a
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2024-06-23 21:43:15 +0300

    fix

--- page.php
+++ page.php
@@ -3,6 +3,9 @@
 /**
  * The template for displaying all pages
  *
+ *
+ * Template Name: main
+ *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
@@ -43,6 +46,7 @@ the_content();
 
 <?php
 get_sidebar();
+
 get_template_part("template-parts/main-page/services-section");
 get_template_part("template-parts/main-page/proposition-section");
 get_template_part("template-parts/main-page/blog-section");
