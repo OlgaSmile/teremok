1c6a7c9697379edf97c1adb0e24d404902c3daad
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2024-07-07 17:31:09 +0300

    service section mobile

--- templates/home/services-section.php
+++ templates/home/services-section.php
@@ -16,6 +16,16 @@ get_template_part("template-parts/section-title", null, array('title' => $servic
 
         <div class="services-section__content">
             <h2 class="services-section__subtitle">Безкоштовні</h2>
+            <div class="services-section__image-mobile">
+                <?php if (get_field('services-section-left-photos')): ?>
+                <?php while (has_sub_field('services-section-left-photos') and get_row_index() <= 2): ?>
+
+                <img class="services-section__image-mobile-item"
+                    src="<?php the_sub_field('services-section-left-photo');?>" />
+                <?php endwhile;?>
+                <?php endif;?>
+
+            </div>
             <ul class="services-section__free">
                 <?php if (get_field('free_services')): ?>
                 <?php while (has_sub_field('free_services')): ?>
@@ -33,6 +43,16 @@ get_template_part("template-parts/section-title", null, array('title' => $servic
 
         <div class="services-section__content">
             <h2 class="services-section__subtitle">Додаткові</h2>
+            <div class="services-section__image-mobile">
+                <?php if (get_field('services-section-right-photos')): ?>
+                <?php while (has_sub_field('services-section-right-photos')): ?>
+
+                <img class="services-section__image-mobile-item"
+                    src="<?php the_sub_field('services-section-right-photo');?>" />
+                <?php endwhile;?>
+                <?php endif;?>
+
+            </div>
             <ul class="services-section__additional">
                 <?php if (get_field('additional_service')): ?>
                 <?php while (has_sub_field('additional_service')): ?>
@@ -56,8 +76,7 @@ get_template_part("template-parts/learn-more-btn", null, array('btn_name' => $le
     </div>
     <div class="services-section__image">
         <?php if (get_field('services-section-right-photos')): ?>
-        <?php while (has_sub_field('services-section-right-photos')): ?>
-
+        <?php while (has_sub_field('services-section-right-photos') and get_row_index() <= 2): ?>
         <img class="services-section__image-item" src="<?php the_sub_field('services-section-right-photo');?>" />
         <?php endwhile;?>
         <?php endif;?>
