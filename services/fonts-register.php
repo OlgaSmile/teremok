476fc4c698e24f9d06957717f1a758bee07a62ad
Olga <112238837+OlgaSmile@users.noreply.github.com>
2024-07-13 12:01:03 +0300

    fix font weight on read-more buttons, add animations for sosials in hero

--- services/fonts-register.php
+++ services/fonts-register.php
@@ -3,6 +3,6 @@
 add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
 
 function add_google_fonts() {
-  wp_enqueue_style( 'google_web_fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@200;400;500;600&family=Montserrat:wght@400;500;600;700&family=Roboto:wght@400;700&display=swap', [], null );
+  wp_enqueue_style( 'google_web_fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@200;400;500;600&family=Montserrat:wght@200;400;500;600;700&family=Roboto:wght@400;700&display=swap', [], null );
 }
  
