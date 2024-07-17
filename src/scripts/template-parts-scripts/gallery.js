87182c19d1b86c6b44db9054839a9e4606f4fed9
Oleksii Shevchenko <94850347+oleksii-she@users.noreply.github.com>
2024-07-17 21:22:42 +0300

    Додав фото колаж

--- src/scripts/template-parts-scripts/gallery.js
+++ src/scripts/template-parts-scripts/gallery.js
@@ -11,14 +11,12 @@
         columnWidth: ".masonry-item",
         percentPosition: true,
       })
-      mobileMasonry.masonry("destroy")
     } else {
       mobileMasonry.masonry({
         itemSelector: ".masonry-mobile-item",
         columnWidth: ".sizer",
         percentPosition: true,
       })
-      desktopMasonry.masonry("destroy")
     }
   }
 
--- templates/home/photo-collage-section.php
+++ templates/home/photo-collage-section.php
@@ -1,11 +1,13 @@
+<?php $collage = get_field('collage'); ?>
 <section class="photo-collage-section">
+  <?php if (!empty($collage)) : ?>
+    <div class="container-collage">
+      <div class="collage">
+        <?php foreach ($collage as $index => $image) : ?>
+          <img class="collage__img" src="<?php echo esc_attr($image['url']); ?>" alt="<?php echo esc_attr(!empty($image['alt']) ? $image['alt'] : 'Колаж'); ?>" />
 
-  <div>
-    <h2>colage</h2>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  <?php endif ?>
 </section>
