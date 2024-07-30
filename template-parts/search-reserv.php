e59c69d2cb0ff3c983471a1d559397a00644db65
Oleksii Shevchenko <94850347+oleksii-she@users.noreply.github.com>
2024-07-31 00:07:13 +0300

    створив розмітку для резева номерів

---
+++ template-parts/search-reserv.php
@@ -0,0 +1,53 @@
+<?php
+
+?>
+<div class="search-reserve-layout">
+  <ul class="search-reserve-layout__list">
+    <li class="search-reserve-layout__item">
+      <div>
+        <h3>Дата заїзду</h3>
+        <svg class="search-reserve-layout__svg-desc" xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 11 7" fill="none">
+          <path d="M5.21862 6.22151C5.37449 6.37578 5.62551 6.37578 5.78138 6.22151L10.8708 1.1843C11.1248 0.932914 10.9468 0.5 10.5894 0.5H0.410592C0.0532349 0.5 -0.124778 0.932913 0.129211 1.1843L5.21862 6.22151Z" fill="#3C4839" />
+        </svg>
+      </div>
+      <div>
+        <span>15 червня 2024</span><svg class="search-reserve-layout__svg-mob" xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 11 7" fill="none">
+          <path d="M5.21862 6.22151C5.37449 6.37578 5.62551 6.37578 5.78138 6.22151L10.8708 1.1843C11.1248 0.932914 10.9468 0.5 10.5894 0.5H0.410592C0.0532349 0.5 -0.124778 0.932913 0.129211 1.1843L5.21862 6.22151Z" fill="#3C4839" />
+        </svg>
+      </div>
+
+    </li>
+    <li class="search-reserve-layout__item">
+      <div>
+        <h3>Дата виїзду</h3>
+        <svg class="search-reserve-layout__svg-desc" xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 11 7" fill="none">
+          <path d="M5.21862 6.22151C5.37449 6.37578 5.62551 6.37578 5.78138 6.22151L10.8708 1.1843C11.1248 0.932914 10.9468 0.5 10.5894 0.5H0.410592C0.0532349 0.5 -0.124778 0.932913 0.129211 1.1843L5.21862 6.22151Z" fill="#3C4839" />
+        </svg>
+      </div>
+      <div>
+        <span>15 червня 2024</span><svg class="search-reserve-layout__svg-mob" xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 11 7" fill="none">
+          <path d="M5.21862 6.22151C5.37449 6.37578 5.62551 6.37578 5.78138 6.22151L10.8708 1.1843C11.1248 0.932914 10.9468 0.5 10.5894 0.5H0.410592C0.0532349 0.5 -0.124778 0.932913 0.129211 1.1843L5.21862 6.22151Z" fill="#3C4839" />
+        </svg>
+      </div>
+
+    </li>
+    <li class="search-reserve-layout__item">
+      <div>
+        <h3>Кількість гостей</h3>
+        <svg class="search-reserve-layout__svg-desc" xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 11 7" fill="none">
+          <path d="M5.21862 6.22151C5.37449 6.37578 5.62551 6.37578 5.78138 6.22151L10.8708 1.1843C11.1248 0.932914 10.9468 0.5 10.5894 0.5H0.410592C0.0532349 0.5 -0.124778 0.932913 0.129211 1.1843L5.21862 6.22151Z" fill="#3C4839" />
+        </svg>
+      </div>
+      <div>
+        <span>4 гостя</span><svg class="search-reserve-layout__svg-mob" xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 11 7" fill="none">
+          <path d="M5.21862 6.22151C5.37449 6.37578 5.62551 6.37578 5.78138 6.22151L10.8708 1.1843C11.1248 0.932914 10.9468 0.5 10.5894 0.5H0.410592C0.0532349 0.5 -0.124778 0.932913 0.129211 1.1843L5.21862 6.22151Z" fill="#3C4839" />
+        </svg>
+      </div>
+
+    </li>
+  </ul>
+
+  <button type="button" class="search-reserve-layout__btn">
+    <span> Знайти й забронювати</span>
+  </button>
+</div>
