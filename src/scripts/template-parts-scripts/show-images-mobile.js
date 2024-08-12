c3e9bd562cb95c4ffbc0f73e1adb544c3c522ea5
Oleksii Shevchenko <94850347+oleksii-she@users.noreply.github.com>
2024-08-12 17:51:16 +0300

    add swiper image, fix title

---
+++ src/scripts/template-parts-scripts/show-images-mobile.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("show images")
+  if (window.innerWidth <= 575.98) {
+    const progressCircle = document.querySelector(".autoplay-progress svg")
+    const progressContent = document.querySelector(".autoplay-progress span")
+    new Swiper(".swiper-image-mobile", {
+      spaceBetween: 0,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      loop: true,
+      on: {
+        autoplayTimeLeft(s, time, progress) {
+          progressCircle.style.setProperty("--progress", 1 - progress)
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`
+        },
+      },
+    })
+  }
+})
