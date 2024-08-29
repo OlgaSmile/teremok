4e1db4fe2daa6d36343326e56118bea3af121eba
Oleksii Shevchenko <94850347+oleksii-she@users.noreply.github.com>
2024-08-29 23:07:51 +0300

    add style booking
    a

---
+++ templates/search-result.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: search-result
+ */
+
+get_header();
+
+
+?>
+
+<main class="">
+
+  <?php if (empty($hero_reviews_image)) {
+
+    get_template_part("template-parts/secondary-hero-section", null, ['img_array' => $hero_reviews_image, "title_section" => get_the_title()]);
+  } ?>
+
+  <section class="">
+
+    <div class="site-content-wrapper ">
+      <?php echo do_shortcode('[mphb_search_results class="search-result"]'); ?>
+    </div>
+
+    </div>
+
+
+  </section>
+</main>
+
+
+<?php get_footer(); ?>
