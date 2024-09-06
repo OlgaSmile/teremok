179c70ed566bae5b65bd1e308c67075c85649574
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2024-09-06 22:58:29 +0300

    add accommodations page

--- src/scripts/template-parts-scripts/booking-search.js
+++ src/scripts/template-parts-scripts/booking-search.js
@@ -1,104 +1,104 @@
 function getNightPriceText(number) {
   if (!number) {
-    return "за ніч"
+    return "за ніч";
   }
-  let text = "за "
+  let text = "за ";
 
-  const lastDigit = number % 10
-  const lastTwoDigits = number % 100
+  const lastDigit = number % 10;
+  const lastTwoDigits = number % 100;
 
   if (lastTwoDigits >= 11 && lastTwoDigits <= 14) {
-    text += number + " ночей"
+    text += number + " ночей";
   } else if (lastDigit === 1) {
-    text += number + " ніч"
+    text += number + " ніч";
   } else if (lastDigit >= 2 && lastDigit <= 4) {
-    text += number + " ночі"
+    text += number + " ночі";
   } else {
-    text += number + " ночей"
+    text += number + " ночей";
   }
 
-  return text
+  return text;
 }
 
 jQuery(document).ready(function ($) {
-  const searchInDate = $(".mphb_sc_search-check-in-date > label")
-  const searchOutDate = $(".mphb_sc_search-check-out-date > label")
-  const searchAdults = $(".mphb_sc_search-adults > label")
+  const searchInDate = $(".mphb_sc_search-check-in-date > label");
+  const searchOutDate = $(".mphb_sc_search-check-out-date > label");
+  const searchAdults = $(".mphb_sc_search-adults > label");
 
-  const pathname = window.location.pathname
+  const pathname = window.location.pathname;
 
-  if (searchInDate) searchInDate.text("Дата заїзду")
-  if (searchOutDate) searchOutDate.text("Дата виїзду")
-  if (searchAdults) searchAdults.text("Кількість гостей")
+  if (searchInDate) searchInDate.text("Дата заїзду");
+  if (searchOutDate) searchOutDate.text("Дата виїзду");
+  if (searchAdults) searchAdults.text("Кількість гостей");
 
-  const formBox = $(".accommodation-form-box")
+  const formBox = $(".accommodation-form-box");
 
-  const FormTitle = $(".mphb-reservation-form-title")
-  const form = $(".mphb-booking-form")
-  const searchButton = $(".mphb_sc_search-submit-button-wrapper > input")
-  searchButton.val("Знайти й забронювати")
+  const FormTitle = $(".mphb-reservation-form-title");
+  const form = $(".mphb-booking-form");
+  const searchButton = $(".mphb_sc_search-submit-button-wrapper > input");
+  searchButton.val("Знайти й забронювати");
 
-  formBox.append(FormTitle)
-  formBox.append(form)
+  formBox.append(FormTitle);
+  formBox.append(form);
 
   $(".mphb-room-type").each(function () {
-    const searchWrapper = $(this)
+    const searchWrapper = $(this);
 
-    const priceNight = $(".mphb-price-period")
-    const reserveBtn = $(".mphb-book-button")
-    const priceText = $(".mphb-regular-price > strong")
+    const priceNight = $(".mphb-price-period");
+    const reserveBtn = $(".mphb-book-button");
+    const priceText = $(".mphb-regular-price > strong");
 
-    const detaliesLink = $(".mphb-view-details-button")
+    const detaliesLink = $(".mphb-view-details-button");
 
     if (detaliesLink) {
-      detaliesLink.text("Детальніше")
+      detaliesLink.text("Детальніше");
     }
 
     if (priceText) {
-      priceText.text("Ціна")
+      priceText.text("Ціна");
     }
 
-    const night = priceNight.text().split(" ")[1]
+    const night = priceNight.text().split(" ")[1];
 
     if (Number(night)) {
-      const result = getNightPriceText(Number(night))
+      const result = getNightPriceText(Number(night));
 
-      priceNight.text(result)
+      priceNight.text(result);
     } else {
-      priceNight.text("за ніч")
+      priceNight.text("за ніч");
     }
 
-    reserveBtn.text("Забронювати")
+    reserveBtn.text("Забронювати");
     if (
       pathname === "/rezul-taty-poshuku/" ||
       pathname === "/teremok/rezul-taty-poshuku/"
     ) {
-      searchButton.val("Змінити й забронювати")
-      const title = searchWrapper.find(".mphb-room-type-title").detach()
+      searchButton.val("Змінити й забронювати");
+      const title = searchWrapper.find(".mphb-room-type-title").detach();
       const details = searchWrapper
         .find(".mphb-view-details-button-wrapper")
-        .detach()
-      const paragraph = searchWrapper.find("p").detach()
+        .detach();
+      const paragraph = searchWrapper.find("p").detach();
 
       const attributes = searchWrapper
         .find(".mphb-loop-room-type-attributes")
-        .detach()
-      const book = searchWrapper.find(".mphb-reserve-room-section").detach()
+        .detach();
+      const book = searchWrapper.find(".mphb-reserve-room-section").detach();
 
       const descriptionWrapper = $("<div></div>").addClass(
-        "description-wrapper",
-      )
-      const topWrapper = $("<div></div>").addClass("description-top-wrapper")
-      const btnWrapper = $("<div></div>").addClass("description-btn-wrapper")
-      topWrapper.append(title)
-      topWrapper.append(paragraph)
-      topWrapper.append(attributes)
-      btnWrapper.append(details)
-      btnWrapper.append(book)
-      descriptionWrapper.append(topWrapper)
-      descriptionWrapper.append(btnWrapper)
+        "description-wrapper"
+      );
+      const topWrapper = $("<div></div>").addClass("description-top-wrapper");
+      const btnWrapper = $("<div></div>").addClass("description-btn-wrapper");
+      topWrapper.append(title);
+      topWrapper.append(paragraph);
+      topWrapper.append(attributes);
+      btnWrapper.append(details);
+      btnWrapper.append(book);
+      descriptionWrapper.append(topWrapper);
+      descriptionWrapper.append(btnWrapper);
 
-      searchWrapper.append(descriptionWrapper)
+      searchWrapper.append(descriptionWrapper);
     }
-  })
-})
+  });
+});
