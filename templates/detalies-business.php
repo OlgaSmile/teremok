d8c30152525e6f217c7bc0b17eaeb91757fcca41
Oleksii Shevchenko <94850347+oleksii-she@users.noreply.github.com>
2024-11-09 13:09:31 +0200

    add animation

--- templates/detalies-business.php
+++ templates/detalies-business.php
@@ -25,7 +25,9 @@ $detalies_telegram = get_field('detalies_telegram');
 $detalies_viber = get_field('detalies_viber');
 
 $detalies_videos_title = get_field('detalies_videos_title');
-$detalies_videos = get_field('detalies_videos')
+$detalies_videos = get_field('detalies_videos');
+
+$google_maps_address = get_field('google_maps_address', 'option');
 ?>
 
 <main>
@@ -120,7 +122,7 @@ $detalies_videos = get_field('detalies_videos')
 
         <?php get_template_part("template-parts/logo-detalies"); ?>
 
-        <a href="" class="section-hero-detalies__location"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <a href="<?php echo esc_url($google_maps_address['url']) ?>" class="section-hero-detalies__location" target="_blank"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12.0001 2C7.58908 2 4.00008 5.589 4.00008 9.995C3.97108 16.44 11.6961 21.784 12.0001 22C12.0001 22 20.0291 16.44 20.0001 10C20.0001 5.589 16.4111 2 12.0001 2ZM12.0001 14C9.79008 14 8.00008 12.21 8.00008 10C8.00008 7.79 9.79008 6 12.0001 6C14.2101 6 16.0001 7.79 16.0001 10C16.0001 12.21 14.2101 14 12.0001 14Z" fill="white" />
             </svg>
             <span><?php echo get_field('location_address', 'options') ?></span></a>
