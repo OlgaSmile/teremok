bc91f5480bae16217729b99c89bfe92890daa6ca
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2024-11-12 22:08:07 +0200

    fix location section

--- template-parts/location-section.php
+++ template-parts/location-section.php
@@ -5,15 +5,21 @@
 $location_title = get_field('location_title', 'options');
 $google_maps_address = get_field('google_maps_address', 'options');
 $location_image = get_field('location_map', 'options');
+$location_pin = get_field('location_pin', 'options');
 
 get_template_part("template-parts/section-title", null, array('title' => $location_title));?>
     <h2 class="location-section__subtitle"><?php the_field('location_subtitle', 'options');?></h2>
     <p class="location-section__address"><?php the_field('location_address', 'options');?></p>
     <div class="location-section__map">
-
-        <a href="<?php echo esc_url($google_maps_address['url']); ?>"
+        <a class="location-section__map-link" href="<?php echo esc_url($google_maps_address['url']); ?>"
             target="<?php echo $google_maps_address['target']; ?>">
-            <img src="<?php echo $location_image; ?>" />
+            <img class="location-section__map-image" src="<?php echo $location_image; ?>" />
         </a>
+        <a class="location-section__map-pin" href="<?php echo esc_url($google_maps_address['url']); ?>"
+            target="<?php echo $google_maps_address['target']; ?>">
+            <img src="<?php echo $location_pin; ?>" />
+            <span>Вишка</span>
+        </a>
+
     </div>
 </section>
