422bffd477b04d3d17ba12dbbf4e7e1e32bc753a
Oleksii Shevchenko <94850347+oleksii-she@users.noreply.github.com>
2024-11-26 14:57:38 +0200

    fix

--- templates/reservation-confirmed.php
+++ templates/reservation-confirmed.php
@@ -21,9 +21,9 @@ $main_link = get_field('main_page_link', 'options')
 
       get_template_part("template-parts/section-title", null, ['title' => $thanks_feedback]); ?>
     </div>
-
-    <h3 id="positive-response"><?php echo $reservation_confirmed_text ?></h3>
-
+    <?php if (!empty($reservation_confirmed_text)): ?>
+      <h3 id="positive-response"><?php echo $reservation_confirmed_text ?></h3>
+    <?php endif ?>
     <?php if (!empty($main_link)): ?>
       <a class="secondary_button booking_btn-footer" href="<?php echo home_url() ?>"><?php echo $main_link ?></a>
     <?php endif ?>
