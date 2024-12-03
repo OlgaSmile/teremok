d1d3b7463d8fa80ac14adb1f05a31d9cce5c7e18
Oleksii Shevchenko <94850347+oleksii-she@users.noreply.github.com>
2024-12-03 16:55:23 +0200

    home-page fix

--- src/scripts/template-parts-scripts/one-apartment.js
+++ src/scripts/template-parts-scripts/one-apartment.js
@@ -27,4 +27,32 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     },
   })
+
+  new Swiper(".one-apartment__services--mobile", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    speed: 1000,
+    loop: true,
+    lazy: {
+      loadOnTransitionStart: true,
+      loadPrevNext: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      slideToClickedSlide: true,
+    },
+    navigation: {
+      nextEl: ".custom-next-icon",
+      prevEl: ".custom-prev-icon",
+    },
+    breakpoints: {
+      576: {
+        initialSlide: 1,
+        spaceBetween: 0,
+        centeredSlides: true,
+        slidesPerView: "auto",
+      },
+    },
+  })
 })
